55f7eb500b64a88a4678cbde3ecd72443703d091
Christian Bäumlisberger <C.Baeumlisberger@live.com>
2023-08-08 20:27:10 +0200

    Rename files based on date taken

---
+++ PhotoViewer.App/Views/Dialogs/PrefixFilesByDateDialog.xaml.cs
@@ -0,0 +1,15 @@
+﻿using PhotoViewer.App.Utils;
+using PhotoViewer.App.Views.Dialogs;
+using PhotoViewer.Core.ViewModels.Dialogs;
+
+namespace PhotoViewer.App.Views;
+
+[ViewRegistration(typeof(PrefixFilesByDateDialogModel))]
+public sealed partial class PrefixFilesByDateDialog : MultiViewDialogBase, IMVVMControl<PrefixFilesByDateDialogModel>
+{
+    public PrefixFilesByDateDialog()
+    {
+        this.InitializeComponentMVVM();
+    }
+
+}
---
+++ PhotoViewer.Core/Commands/PrefixFilesByDateCommand.cs
@@ -0,0 +1,38 @@
+﻿using PhotoViewer.App.Services;
+using PhotoViewer.Core.Models;
+using PhotoViewer.Core.Utils;
+using PhotoViewer.Core.ViewModels.Dialogs;
+using Windows.System;
+
+namespace PhotoViewer.Core.Commands;
+
+internal interface IPrefixFilesByDateCommand : IAcceleratedCommand { }
+
+internal class PrefixFilesByDateCommand : AsyncCommandBase, IPrefixFilesByDateCommand
+{
+    public VirtualKey AcceleratorKey => VirtualKey.N;
+
+    public VirtualKeyModifiers AcceleratorModifiers => VirtualKeyModifiers.Control;
+
+
+    private readonly ApplicationSession session;
+
+    private readonly ApplicationSettings settings;
+
+    private readonly IDialogService dialogService;
+
+    private readonly IMetadataService metadataService;
+
+    public PrefixFilesByDateCommand(ApplicationSession session, ApplicationSettings settings, IDialogService dialogService, IMetadataService metadataService)
+    {
+        this.session = session;
+        this.settings = settings;
+        this.dialogService = dialogService;
+        this.metadataService = metadataService;
+    }
+
+    protected override async Task OnExecuteAsync()
+    {
+        await dialogService.ShowDialogAsync(new PrefixFilesByDateDialogModel(session.Files, settings, metadataService));
+    }
+}
