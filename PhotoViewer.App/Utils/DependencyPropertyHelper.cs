f753cf5b885f121c1e14c5eeeb908114d6d7270d
sbaeumlisberger <s.baeumlisberger@live.de>
2025-11-15 19:08:03 +0100

    Fix order of arguments passed to DependencyProperty.Register

--- PhotoViewer.App/Utils/DependencyPropertyHelper.cs
+++ PhotoViewer.App/Utils/DependencyPropertyHelper.cs
@@ -5,27 +5,27 @@ using System.Runtime.CompilerServices;
 
 namespace PhotoViewer.App.Utils;
 
-public class DependencyPropertyHelper<TDependencyObject>
+public static class DependencyPropertyHelper<TDependencyObject>
 {
-    public static DependencyProperty Register<T>(string propertyName, T defaultValue)
+    public static DependencyProperty Register<TProperty>(string propertyName, TProperty defaultValue)
     {
-        return DependencyProperty.Register(propertyName, typeof(TDependencyObject), typeof(T), new PropertyMetadata(defaultValue));
+        return DependencyProperty.Register(propertyName, typeof(TProperty), typeof(TDependencyObject), new PropertyMetadata(defaultValue));
     }
 
-    public static DependencyProperty Register<T>(string propertyName, T defaultValue, Action<TDependencyObject, DependencyPropertyChangedEventArgs> propertyChangedCallback)
+    public static DependencyProperty Register<TProperty>(string propertyName, TProperty defaultValue, Action<TDependencyObject, DependencyPropertyChangedEventArgs> propertyChangedCallback)
     {
         var metadata = new PropertyMetadata(defaultValue, (obj, args) => propertyChangedCallback((TDependencyObject)(object)obj, args));
-        return DependencyProperty.Register(propertyName, typeof(TDependencyObject), typeof(T), metadata);
+        return DependencyProperty.Register(propertyName, typeof(TProperty), typeof(TDependencyObject), metadata);
     }
 
     public static DependencyProperty RegisterAttached<TProperty>(TProperty defaultValue, [CallerMemberName] string propertyName = "")
     {
-        return DependencyProperty.RegisterAttached(propertyName.RemoveEnd("Property"), typeof(TDependencyObject), typeof(TProperty), new PropertyMetadata(defaultValue));
+        return DependencyProperty.RegisterAttached(propertyName.RemoveEnd("Property"), typeof(TProperty), typeof(TDependencyObject), new PropertyMetadata(defaultValue));
     }
 
     public static DependencyProperty RegisterAttached<TProperty>(TProperty defaultValue, Action<DependencyObject, DependencyPropertyChangedEventArgs> propertyChangedCallback, [CallerMemberName] string propertyName = "")
     {
         var metadata = new PropertyMetadata(defaultValue, (obj, args) => propertyChangedCallback(obj, args));
-        return DependencyProperty.RegisterAttached(propertyName.RemoveEnd("Property"), typeof(TDependencyObject), typeof(TProperty), metadata);
+        return DependencyProperty.RegisterAttached(propertyName.RemoveEnd("Property"), typeof(TProperty), typeof(TDependencyObject), metadata);
     }
 }
